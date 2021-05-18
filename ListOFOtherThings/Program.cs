30eccfc0be5e17e96add756df7f50a5c8495ad2f
nahidtribedev <83621077+nahidtribedev@users.noreply.github.com>
2021-05-18 15:45:22 +1000

    All codes have been addedd

---
+++ ListOFOtherThings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace ListOFOtherThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            while (fibonacciNumbers.Count < 20)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+            }
+           foreach (int item in fibonacciNumbers)
+         {
+                Console.WriteLine(item);
+            }
+           // Console.WriteLine(previous);
+        }
+    }
+}
