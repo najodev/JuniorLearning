8b047670a1dd938f72c420e9f6d112713964d5dc
Nash Barden <nash@najo.dev>
2022-01-12 15:45:52 +1100

    Inheritance

---
+++ W1-M7-inheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using classes;
+
+/// <summary> 
+/// Creates a new WorkItem called item
+/// </summary>
+WorkItem item = new WorkItem("Fix Bugs", "Fix all bugs in my git pull request", new TimeSpan(3, 4, 0, 0));
+
+/// <summary> 
+/// create a new change request called change 
+/// </summary>
+ChangeRequest change = new ChangeRequest("Change Base Class Design", "Add members to the class", new TimeSpan(4, 0, 0), 1);
+
+Console.WriteLine(item.ToString());
+
+change.Update("Change the Design of the base class", new TimeSpan(4, 0, 0));
+
+Console.WriteLine(change.ToString());
+
