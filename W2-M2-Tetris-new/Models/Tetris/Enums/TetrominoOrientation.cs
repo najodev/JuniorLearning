57495b25b417b43e1b88bc3c7560294a8c6a43eb
Nash Barden <nash.barden@tribetech.com.au>
2022-01-24 11:03:34 +1100

    Testing

---
+++ W2-M2-Tetris-new/Models/Tetris/Enums/TetrominoOrientation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace W1_M2_Tetris.Models.Tetris.Enums
+{
+    public enum TetrominoOrientation
+    {
+        UpDown,
+        LeftRight,
+        DownUp,
+        RightLeft
+    }
+}
