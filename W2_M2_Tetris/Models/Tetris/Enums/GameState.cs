af58345d4ffe1da4a8bc0bd9a8c2d03c7446d7e4
Nash Barden <nash.barden@tribetech.com.au>
2022-01-24 11:05:57 +1100

    should be fixed now

---
+++ W2_M2_Tetris/Models/Tetris/Enums/GameState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace W1_M2_Tetris.Models.Tetris.Enums
+{
+    /// <summary>
+    /// Represents the current state of the Tetris game
+    /// </summary>
+    public enum GameState
+    {
+        NotStarted, 
+        Playing, //Game playing normally
+        GameOver
+    }
+}
